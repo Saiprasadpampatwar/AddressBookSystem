f35f5dc4a66ad845b7f775b537eb63732b33b8e3
SaiprasadPampatwar <pampatwarsaiprasad321@gmail.com>
2020-11-20 12:01:28 +0530

    Added Atributes of Address Book

--- AddressBookMain.java
+++ AddressBookMain.java
@@ -4,8 +4,18 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Welcome to Address Book Program");
-
-
+		
 	}
 
+}
+
+ class Person{
+	String firstname;
+	String lastname;
+	String address;
+	String city;
+	String state;
+	int zip;
+	int phoneno;
+	String email;	
 }
